5dc8b2971a1c6f1b63b82d8a2ea2956078a61373
Fralch <ingfralch@gmail.com>
2023-11-29 10:29:26 -0500

    funciono

--- 4Unacajadentrodeotra.js
+++ 4Unacajadentrodeotra.js
@@ -3,6 +3,13 @@ const boxes = [
     { l: 2, w: 2, h: 2 },
     { l: 3, w: 1, h: 3 }
   ] // false
+
+// const boxes = [
+//   { l: 1, w: 1, h: 1 },
+//   { l: 3, w: 3, h: 3 },
+//   { l: 2, w: 2, h: 2 }
+// ]// true
+
  
 
 // como puedo saber si una caja cabe dentro de otra caja  los valores de las cajas son l = largo, w = ancho, h = alto
@@ -13,15 +20,23 @@ const fitsInOneBox = (boxes) => {
     let result = false; 
 
     for (let i = 0; i < boxes.length; i++) {
-      if(boxes[i+1] !== undefined){
+      if(boxes[i+1]){
         if (boxes[i].l < boxes[i+1]?.l && boxes[i].w < boxes[i+1]?.w && boxes[i].h < boxes[i+1]?.h) {
           result = true;
         } else {
            result = false;
-       }
+        }
+      }
+      if(boxes[i-1]){
+        if (boxes[i].l < boxes[i-1]?.l && boxes[i].w < boxes[i-1]?.w && boxes[i].h < boxes[i-1]?.h) {
+          result = true;
+        } else {
+           result = false;
+        }
       }
       
     }
+    return result;
    
 }
 
