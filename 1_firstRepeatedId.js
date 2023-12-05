b5b61c2f5c1245bcd0ef44e5ce06351a7435964c
Fralch <ingfralch@gmail.com>
2023-12-05 16:38:56 -0500

    recordando map

--- 1_firstRepeatedId.js
+++ 1_firstRepeatedId.js
@@ -25,3 +25,39 @@ function firstRepeatedId(gifts) {
 
 const x = firstRepeatedId(giftIds);
 console.log(x);
+
+
+// mejorado por chatGpt
+// Array de identificadores de regalos
+const giftIds2 = [2, 1, 3, 5, 3, 2];
+
+// Función que encuentra el primer regalo repetido en el array
+function firstRepeatedId(gifts) {
+    // Mapa para almacenar los índices de los regalos que ya hemos visto
+    const indexMap = new Map();
+
+    // Itera a través de los regalos
+    for (let i = 0; i < gifts.length; i++) {
+        // Obtiene el identificador del regalo actual
+        console.log(indexMap);
+        const gift = gifts[i];
+
+        // Verifica si ya hemos visto este regalo
+        if (indexMap.has(gift)) {
+            // Si sí, devuelve el identificador del regalo repetido
+            return gift;
+        }
+
+        // Si no hemos visto el regalo, almacenamos su índice en el mapa
+        indexMap.set(gift, i);
+    }
+
+    // Si no se encuentra ningún regalo repetido, devuelve -1
+    return -1;
+}
+
+// Llama a la función con el array de identificadores de regalos y almacena el resultado
+const result = firstRepeatedId(giftIds2);
+
+// Imprime el resultado en la consola
+console.log(result);
